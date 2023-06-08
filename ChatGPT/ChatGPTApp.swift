a8311d91fd042c539a41bbdf8a9485b3f9d1fd8d
Toni K. Turk <kocjanturk@gmail.com>
2023-06-08 08:10:59 +0200

    Initial Commit

---
+++ ChatGPT/ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTApp.swift
+//  ChatGPT
+//
+//  Created by Toni K. Turk on 08/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
