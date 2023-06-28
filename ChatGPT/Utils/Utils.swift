021e12172b7a628af0c27921ac6fee3d5141d369
Toni K. Turk <kocjanturk@gmail.com>
2023-06-28 11:25:05 +0200

    Partial support for markdown parsing

--- ChatGPT/Utils/Utils.swift
+++ ChatGPT/Utils/Utils.swift
@@ -214,13 +214,7 @@ func messageToAttributedString(
     var substr = AttributedString(prefix + header + "\n\n")
     substr.setAttributes(container)
     string.append(substr)
-    
-    container = AttributeContainer()
-    container.foregroundColor = .white
-    container.font = .systemFont(ofSize: 14)
-    substr = AttributedString(message.content!)
-    substr.setAttributes(container)
-    string.append(substr)
+    string.append(parseMarkdown(message.content ?? ""))
     return string
   }
   
@@ -251,6 +245,71 @@ func messageToAttributedString(
     )
   case _:
     fatalError()
+  }
+}
+
+func parseMarkdown(
+  _ markdown: String,
+  allowsExtendedAttributes: Bool = false,
+  interpretedSyntax: AttributedString.MarkdownParsingOptions.InterpretedSyntax = .inlineOnlyPreservingWhitespace,
+  failurePolicy: AttributedString.MarkdownParsingOptions.FailurePolicy = .returnPartiallyParsedIfPossible,
+  primaryForegroundColor: Color? = nil
+) -> AttributedString {
+  do {
+    var attributedString = try AttributedString(
+      markdown: markdown, 
+      options: .init(
+        allowsExtendedAttributes: allowsExtendedAttributes, 
+        interpretedSyntax: interpretedSyntax, 
+        failurePolicy: failurePolicy
+      )
+    )
+    
+    var globalContainer = AttributeContainer()
+    globalContainer.font = .systemFont(ofSize: 14)
+    if let color = primaryForegroundColor {
+      globalContainer.foregroundColor = color
+    }
+    attributedString.mergeAttributes(globalContainer, mergePolicy: .keepNew)
+    
+    markdown.ranges(of: /###(.+?)\n/).forEach {
+      guard let range1 = attributedString.range(of: markdown[$0]) else { return }
+      let range2 = attributedString.range(of: markdown[$0].dropFirst(4))!
+      var container = AttributeContainer()
+      container.font = .heading3
+      attributedString.replaceSubrange(
+        range1, 
+        with: attributedString[range2].settingAttributes(container)
+      )
+    }
+    markdown.ranges(of: /##(.+?)\n/).forEach {
+      guard let range1 = attributedString.range(of: markdown[$0]) else { return } 
+      let range2 = attributedString.range(of: markdown[$0].dropFirst(3))!
+      var container = AttributeContainer()
+      container.font = .heading2
+      attributedString.replaceSubrange(
+        range1, 
+        with: attributedString[range2].settingAttributes(container)
+      )
+    }
+    markdown.ranges(of: /#(.+?)\n/).forEach {
+      guard let range1 = attributedString.range(of: markdown[$0]) else { return } 
+      let range2 = attributedString.range(of: markdown[$0].dropFirst(2))!
+      var container = AttributeContainer()
+      container.font = .heading1
+      //      let paragraphStyle = NSMutableParagraphStyle()
+      //      paragraphStyle.paragraphSpacing = 40
+      //      paragraphStyle.lineSpacing = 40
+      //      paragraphStyle.headIndent = 100
+      //      container.paragraphStyle = paragraphStyle
+      attributedString.replaceSubrange(
+        range1, 
+        with: attributedString[range2].settingAttributes(container)
+      )
+    }
+    return attributedString
+  } catch {
+    return .init(markdown)
   }
 }
 
